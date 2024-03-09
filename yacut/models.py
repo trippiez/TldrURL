8394a6e698dd8f2e409ac3b12cbef0c2aeda32b4
Tom Bulmer <tombulmer@MacBook-Pro.local>
2024-03-09 11:26:00 +0200

    lego base.html

--- yacut/models.py
+++ yacut/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from yacut import db
+import string
+from random import choices
 
 
 class URLMap(db.Model):
@@ -8,3 +10,15 @@ class URLMap(db.Model):
     original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16), unique=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.short_url = self.get_unique_short_id()
+
+    def get_unique_short_id(self):
+        characters = string.digits + string.ascii_letters
+        short_url = ''.join(choices(characters, k=16))
+
+        link = self.query.filter_by(short=short_url).first()
+        if link:
+            return self.get_unique_short_id
