e3cb97a2840987ffac1f4dfc29d8c41881aa6c34
Tom Bulmer <tombulmer@MacBook-Pro.local>
2024-03-13 07:07:51 +0200

    add setings vars for YP tests

--- settings.py
+++ settings.py
@@ -6,6 +6,9 @@ openapi_file = os.path.join(BASE_DIR, 'openapi.yml')
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'TQDM')
+    SECRET_KEY = 'TQDM'
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SECRET_KEY = os.getenv('SECRET_KEY')
